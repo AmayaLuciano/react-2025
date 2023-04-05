0f4978be991c8ad263d3cbddcfd444037e045d5e
Luciano Amaya <luciano.amayaaa@gmail.com>
2023-04-05 14:49:38 -0300

    Hello, world!

--- pages/index.js
+++ pages/index.js
@@ -40,23 +40,8 @@ export default function Home() {
         </div>
 
         <div className={styles.center}>
-          <Image
-            className={styles.logo}
-            src="/next.svg"
-            alt="Next.js Logo"
-            width={180}
-            height={37}
-            priority
-          />
-          <div className={styles.thirteen}>
-            <Image
-              src="/thirteen.svg"
-              alt="13"
-              width={40}
-              height={31}
-              priority
-            />
-          </div>
+        <h1>React 2025</h1>
+         
         </div>
 
         <div className={styles.grid}>
